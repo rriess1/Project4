fbbdd1d75f748c64b5adb6474ca0a1e4b88e9eef
Robert Riess <rriessl@binghamton.edu>
2019-03-15 22:29:18 -0400

    fina'

--- app/src/main/java/com/example/project4/SnakeActivity.java
+++ app/src/main/java/com/example/project4/SnakeActivity.java
@@ -1,45 +1,47 @@
 package com.example.project4;
 
+
+import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
 
-public class SnakeActivity extends MainActivity {
+public class SnakeActivity extends Activity {
 
-    // Declare an instance of SnakeEngine
-    // We will code this soon
+
     SnakeEngine snakeEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Get the pixel dimensions of the screen
+
         Display display = getWindowManager().getDefaultDisplay();
 
-        // Initialize the result into a Point object
+
         Point size = new Point();
         display.getSize(size);
 
-        // Create a new instance of the SnakeEngine class
+
         snakeEngine = new SnakeEngine(this, size);
 
-        // Make snakeEngine the view of the Activity
+
         setContentView(snakeEngine);
     }
 
-    // Start the thread in snakeEngine
+
     @Override
     protected void onResume() {
         super.onResume();
         snakeEngine.resume();
     }
 
-    // Stop the thread in snakeEngine
+
     @Override
     protected void onPause() {
         super.onPause();
         snakeEngine.pause();
     }
+
 
 }
--- app/src/main/java/com/example/project4/SnakeEngine.java
+++ app/src/main/java/com/example/project4/SnakeEngine.java
@@ -16,70 +16,65 @@ import android.graphics.Color;
 import android.graphics.Paint;
 
 
-
-
 class SnakeEngine extends SurfaceView implements Runnable {
 
-    // Our game thread for the main game loop
+
     private Thread thread = null;
 
-    // To hold a reference to the Activity
+
     private Context context;
 
-    // for plaing sound effects
+
     private SoundPool soundPool;
     private int eat_bob = -1;
     private int snake_crash = -1;
 
-    // For tracking movement Heading
+
     public enum Heading {UP, RIGHT, DOWN, LEFT}
-    // Start by heading to the right
+
     private Heading heading = Heading.RIGHT;
 
-    // To hold the screen size in pixels
+
     private int screenX;
     private int screenY;
 
-    // How long is the snake
+
     private int snakeLength;
 
-    // Where is Bob hiding?
+
     private int bobX;
     private int bobY;
 
-    // The size in pixels of a snake segment
+
     private int blockSize;
 
-    // The size in segments of the playable area
-    private final int NUM_BLOCKS_WIDE = 40;
+
+    private final int NUM_BLOCKS_WIDE = 45;
     private int numBlocksHigh;
 
-    // Control pausing between updates
-    private long nextFrameTime;
-    // Update the game 10 times per second
-    private final long FPS = 10;
-    // There are 1000 milliseconds in a second
-    private final long MILLIS_PER_SECOND = 1000;
-// We will draw the frame much more often
 
-    // How many points does the player have
+    private long nextFrameTime;
+
+    private final long FPS = 10;
+
+    private final long MILLIS_PER_SECOND = 1000;
+
     private int score;
 
-    // The location in the grid of all the segments
+
     private int[] snakeXs;
     private int[] snakeYs;
 
-    // Everything we need for drawing
-// Is the game currently playing?
+
     private volatile boolean isPlaying;
 
-    // A canvas for our paint
+
     private Canvas canvas;
 
-    // Required to use canvas
+
     private SurfaceHolder surfaceHolder;
 
-    // Some paint for our canvas
+
     private Paint paint;
 
     public SnakeEngine(Context context, Point size) {
@@ -90,20 +85,18 @@ class SnakeEngine extends SurfaceView implements Runnable {
         screenX = size.x;
         screenY = size.y;
 
-        // Work out how many pixels each block is
+
         blockSize = screenX / NUM_BLOCKS_WIDE;
-        // How many blocks of the same size will fit into the height
+
         numBlocksHigh = screenY / blockSize;
 
-        // Set the sound up
+
         soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
         try {
-            // Create objects of the 2 required classes
-            // Use m_Context because this is a reference to the Activity
+
             AssetManager assetManager = context.getAssets();
             AssetFileDescriptor descriptor;
 
-            // Prepare the two sounds in memory
             descriptor = assetManager.openFd("get_mouse_sound.ogg");
             eat_bob = soundPool.load(descriptor, 0);
 
@@ -115,15 +108,14 @@ class SnakeEngine extends SurfaceView implements Runnable {
         }
 
 
-        // Initialize the drawing objects
         surfaceHolder = getHolder();
         paint = new Paint();
 
-        // If you score 200 you are rewarded with a crash achievement!
+
         snakeXs = new int[200];
         snakeYs = new int[200];
 
-        // Start the game
+
         newGame();
     }
 
@@ -132,7 +124,7 @@ class SnakeEngine extends SurfaceView implements Runnable {
 
         while (isPlaying) {
 
-            // Update 10 times a second
+
             if(updateRequired()) {
                 update();
                 draw();
@@ -157,18 +149,18 @@ class SnakeEngine extends SurfaceView implements Runnable {
     }
 
     public void newGame() {
-        // Start with a single snake segment
+
         snakeLength = 1;
         snakeXs[0] = NUM_BLOCKS_WIDE / 2;
         snakeYs[0] = numBlocksHigh / 2;
 
-        // Get Bob ready for dinner
+
         spawnBob();
 
-        // Reset the score
+
         score = 0;
 
-        // Setup nextFrameTime so an update is triggered
+
         nextFrameTime = System.currentTimeMillis();
     }
 
@@ -179,30 +171,25 @@ class SnakeEngine extends SurfaceView implements Runnable {
     }
 
     private void eatBob(){
-        //  Got him!
-        // Increase the size of the snake
+
         snakeLength++;
-        //replace Bob
-        // This reminds me of Edge of Tomorrow. Oneday Bob will be ready!
+
         spawnBob();
-        //add to the score
         score = score + 1;
         soundPool.play(eat_bob, 1, 1, 0, 0, 1);
     }
 
     private void moveSnake(){
-        // Move the body
+
         for (int i = snakeLength; i > 0; i--) {
-            // Start at the back and move it
-            // to the position of the segment in front of it
+
             snakeXs[i] = snakeXs[i - 1];
             snakeYs[i] = snakeYs[i - 1];
 
-            // Exclude the head because
-            // the head has nothing in front of it
+
         }
 
-        // Move the head in the appropriate heading
+
         switch (heading) {
             case UP:
                 snakeYs[0]--;
@@ -223,16 +210,16 @@ class SnakeEngine extends SurfaceView implements Runnable {
     }
 
     private boolean detectDeath(){
-        // Has the snake died?
+
         boolean dead = false;
 
-        // Hit the screen edge
+
         if (snakeXs[0] == -1) dead = true;
         if (snakeXs[0] >= NUM_BLOCKS_WIDE) dead = true;
         if (snakeYs[0] == -1) dead = true;
         if (snakeYs[0] == numBlocksHigh) dead = true;
 
-        // Eaten itself?
+
         for (int i = snakeLength - 1; i > 0; i--) {
             if ((i > 4) && (snakeXs[0] == snakeXs[i]) && (snakeYs[0] == snakeYs[i])) {
                 dead = true;
@@ -242,11 +229,8 @@ class SnakeEngine extends SurfaceView implements Runnable {
         return dead;
     }
 
-
-
-
     public void update() {
-        // Did the head of the snake eat Bob?
+
         if (snakeXs[0] == bobX && snakeYs[0] == bobY) {
             eatBob();
         }
@@ -254,7 +238,7 @@ class SnakeEngine extends SurfaceView implements Runnable {
         moveSnake();
 
         if (detectDeath()) {
-            //start again
+
             soundPool.play(snake_crash, 1, 1, 0, 0, 1);
 
             newGame();
@@ -262,12 +246,12 @@ class SnakeEngine extends SurfaceView implements Runnable {
     }
 
     public void draw() {
-        // Get a lock on the canvas
+
         if (surfaceHolder.getSurface().isValid()) {
             canvas = surfaceHolder.lockCanvas();
 
             // Fill the screen with Game Code School blue
-            canvas.drawColor(Color.argb(255, 26, 128, 182));
+            canvas.drawColor(Color.argb(255, 0, 0, 0));
 
             // Set the color of the paint to draw the snake white
             paint.setColor(Color.argb(255, 255, 255, 255));
@@ -285,32 +269,31 @@ class SnakeEngine extends SurfaceView implements Runnable {
                         paint);
             }
 
-            // Set the color of the paint to draw Bob red
+
             paint.setColor(Color.argb(255, 255, 0, 0));
 
-            // Draw Bob
+
             canvas.drawRect(bobX * blockSize,
                     (bobY * blockSize),
                     (bobX * blockSize) + blockSize,
                     (bobY * blockSize) + blockSize,
                     paint);
 
-            // Unlock the canvas and reveal the graphics for this frame
+
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
     }
 
     public boolean updateRequired() {
 
-        // Are we due to update the frame
-        if(nextFrameTime <= System.currentTimeMillis()){
-            // Tenth of a second has passed
 
-            // Setup when the next update will be triggered
+        if(nextFrameTime <= System.currentTimeMillis()){
+
+
+
             nextFrameTime =System.currentTimeMillis() + MILLIS_PER_SECOND / FPS;
 
-            // Return true so that the update and draw
-            // functions are executed
+
             return true;
         }
 
@@ -356,7 +339,4 @@ class SnakeEngine extends SurfaceView implements Runnable {
         }
         return true;
     }
-
-
-
 }
